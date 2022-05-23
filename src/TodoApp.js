9b0bd662c3a14b04fb0acd803ad19b4b59340780
kai <kai@kaiconsulting.co>
2022-05-22 20:15:19 -0600

    add api request to get todos from 3rd party api

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const TodoApp = () => {
     const [ todoInput, setTodoInput ] = useState('');
-
     const [ todos, setTodos ] = useState([]);
+
+    // useEffect is meant for making API requests to GET data and setting initial state
+    // that's coming from a database or some 3rd party service
+    // useEffect is all react life cycle methods in one
+    // by default, useEffect behaves like componentDidMount
+    useEffect(() => {
+        // this is where we do our api fetching
+        // iffy - to have an async function
+        (async () => {
+            const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos');
+            console.log(data);
+            setTodos(data);
+        })();
+    }, []);
 
     return (
         <>
@@ -24,7 +38,7 @@ export const TodoApp = () => {
                 todos.length === 0 ?
                     <h1>No todos yet</h1>
                     :
-                    todos.map(todo => <p>{todo}</p>)
+                    todos.map(todo => <p>{todo.title}</p>)
             }
         </>
     );
